9874ca0bc4b5381bcef433a8f8b13cabb8f511bb
Tayler Blake <tayler.a.blake@gmail.com>
2016-09-14 18:49:21 -0400

    add bicubic basis function image

--- code/scripts/bspline_img.R
+++ code/scripts/bspline_img.R
@@ -2,14 +2,80 @@
 
 
 setwd(file.path("/Users","taylerblake","Documents","Dissertation"))
-source(file.path(getwd(),"code","aux","bsplbase.R"))
+source(file.path(getwd(),"code","fnc","aux","bsplbase.R"))
 
 library(plyr)
 library(dplyr)
 library(rlist)
 library(ggplot2)
 library(tidyr)
+library(splines)
 
+
+
+p1 <- p2 <- 200
+M1.index <- M2.index <- seq(0,1,length.out=200)
+
+oM1 <- outer(rep(1, p2),M1.index)
+B1 <- bsplbase(as.vector(oM1), c(0,1,4,3))
+oM2 <- outer(M2.index, rep(1, p1))
+B2 <- bsplbase(as.vector(oM2), c(0,1,4,3))
+n1 <- ncol(B1)
+n2 <- ncol(B2)	# Compute tensor products for estimated alpha surface
+B1. <- kronecker(B1, t(rep(1, n2)))
+B2. <- kronecker(t(rep(1, n1)), B2)
+B. <- B1. * B2.
+dim(B.)
+
+# Create a function interpolating colors in the range of specified colors
+#jet.colors <- colorRampPalette( c("lightpink","dodgerblue") )
+jet.colors <- colorRampPalette( c("deepskyblue2","green") )
+# Generate the desired number of colors from this palette
+nbcol <- 100
+color <- jet.colors(nbcol)
+# Compute the z-value at the facet centres
+z <- matrix(B.[,25],nrow=length(M1.index),ncol=length(M1.index))
+
+nrz <- nrow(z)
+ncz <- ncol(z)
+
+zfacet <- z[-1, -1] + z[-1, -ncz] + z[-nrz, -1] + z[-nrz, -ncz]
+
+
+# Recode facet z-values into color indices
+facetcol <- cut(zfacet, nbcol)
+par(bg="white")
+b <- bsplbase(seq(0,1,length.out=200),c(0,1,4,3))[,4]
+
+png(filename = file.path(getwd(),"Dissertation TeX","img","bicubic_basis_function.png"))
+persp(M2.index, M1.index, z,
+      xlab = "", ylab = "", zlab = "",theta = 45, phi = 9,
+      shade = 0.55,col=color[facetcol],
+      box=TRUE,d=6,
+      ticktype = "detailed",
+      nticks=5,lty=2,border = NA,
+      cex.axis=0.5,ltheta=120,zlim=c(0,max(b+0.02)))
+persp(M2.index, M1.index, z,
+      xlab = "", ylab = "", zlab = "",theta = 45, phi = 9,
+      shade = 0.55,col=color[facetcol],
+      box=TRUE,d=6,
+      ticktype = "detailed",
+      nticks=5,lty=2,border = NA,
+      cex.axis=0.5,ltheta=120,zlim=c(0,max(b+0.02)))-> res
+
+for (i in seq(0.1,0.6,by=0.1)){
+      lines (trans3d(x=M1.index, y = 1, z = i, pmat = res),lty=3,col="gray84")
+      lines (trans3d(x=0, y = M1.index, z = i, pmat = res),lty=3,col="gray84")
+}
+for(i in seq(0.2,1,by=0.2)){
+      lines (trans3d(x=0, y = i, z = seq(0,max(b)+0.02,by=0.01), pmat = res),lty=3,col="gray84")
+      lines (trans3d(x=i, y = 1, z = seq(0,max(b)+0.02,by=0.01), pmat = res),lty=3,col="gray84")
+}
+lines(trans3d(x=0, y = M2.index, z = b, pmat = res))
+lines(trans3d(x=M1.index, y = 1, z = b, pmat = res))
+dev.off()
+############################################################################################
+############################################################################################
 ## make a 3d surface plot of a handful of cubic b-spline functions
 
 B <- bsplbase(seq(0,1,by=0.01),bpars=c(0,1,12,3)) 
