79d0e359e672e89b22d3d3d21673a73a151b548f
Tayler Blake <tayler.a.blake@gmail.com>
2017-05-04 18:26:28 -0400

    add case checking to allow for y_aug to be null

--- code/lib/fit_cholesky_PS.R
+++ code/lib/fit_cholesky_PS.R
@@ -10,9 +10,14 @@ fit_cholesky_PS <- function(Y,
                             lambda_m,
                             lambda_ridge){
       N.subjects <- nrow(Y)
+      if (!is.null(y_aug)) {
+            yVec <- as.vector(c(y_aug,
+                                t(Y[,-1])))
+      }
       
-      yVec <- as.vector(c(y_aug,
-                        t(Y[,-1])))
+      if (is.null(y_aug)) {
+            yVec <- as.vector(t(Y[,-1]))
+      }
       Pen <- rbind(lambda_l*P_l,
                    lambda_m*P_m,
                    lambda_ridge*diag(ncol(P_l)))
